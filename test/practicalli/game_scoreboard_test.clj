369487df0b93f529f95277350cf12c2f97dd9957
John Practicalli <250870+practicalli-john@users.noreply.github.com>
2021-08-22 12:55:44 +0100

    new project: Clojure CLI tools app template
    clojure -X:project/new :template app :name practicalli.game-scoreboard

---
+++ test/practicalli/game_scoreboard_test.clj
@@ -0,0 +1,7 @@
+(ns practicalli.game-scoreboard-test
+  (:require [clojure.test :refer :all]
+            [practicalli.game-scoreboard :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
